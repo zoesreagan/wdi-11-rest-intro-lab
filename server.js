ad4602c09e8f752844dd20b66f7cef9c38f3f96b
Zoe <zoereagan@gmail.com>
2018-04-09 15:01:52 -0500

    made index route

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require ('express')
 const app = express();
 
+const superheroes = ['batman', 'superman', 'hulk']
+
+app.get('/superheroes', (req, res) => {
+  res.send(superheroes);
+})
+
+
+
 app.listen(3000, () => {
   console.log("here to save the day");
 });
